bd56d21fce9978c538e8f4a1d58a7bcf8a63110e
LaRita Robinson <laritakr@users.noreply.github.com>
2024-04-12 13:49:35 -0400

    Fixes more solr uri issues (#2182)
    Refs https://github.com/samvera/hyku/issues/2177

--- lib/valkyrie/indexing/solr/indexing_adapter_decorator.rb
+++ lib/valkyrie/indexing/solr/indexing_adapter_decorator.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
-# Override Hyrax v5.0 to avoid incorrect default connection
+# Override Hyrax v5.0 to avoid incorrect default connection. Because of the way
+# Hyku uses SolrEndpoint, the configuration isn't always appropriate. At times,
+# it falls back into the Valkyrie IndexingAdapter instead of Hyrax's, and
+# loses the connection.
 # TODO: create and initialize a Hyku version of the indexing adapter
 module Valkyrie
   module Indexing
     module Solr
       module IndexingAdapterDecorator
+        ##
+        # @param connection [RSolr::Client] The RSolr connection to index to.
+        def initialize(connection: ::SolrEndpoint.new.connection)
+          @connection = connection
+        end
+
         def default_connection
-          ::SolrEndpoint.new.connection
+          @connection = ::SolrEndpoint.new.connection
+        end
+
+        def add_documents(*args)
+          self.connection = default_connection
+          super(*args)
         end
       end
     end
