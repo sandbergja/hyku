9a873c64ff399f34d2ebad9fb64601534f15f29a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-16 14:17:28 -0400

    Add conditional bulkrax_identifier

--- app/indexers/app_indexer.rb
+++ app/indexers/app_indexer.rb
@@ -20,7 +20,7 @@ class AppIndexer < Hyrax::WorkIndexer
       solr_doc['title_ssi'] = SortTitle.new(object.title.first).alphabetical
       solr_doc['depositor_ssi'] = object.depositor
       solr_doc['creator_ssi'] = object.creator&.first
-      solr_doc['bulkrax_identifier_tesim'] = object.bulkrax_identifier
+      solr_doc['bulkrax_identifier_tesim'] = object.bulkrax_identifier if object.respond_to?(:bulkrax_identifier)
       solr_doc['account_cname_tesim'] = Site.instance&.account&.cname
       solr_doc['account_institution_name_ssim'] = Site.instance.institution_label
       solr_doc['all_text_tsimv'] = full_text(object.file_sets.first&.id)
