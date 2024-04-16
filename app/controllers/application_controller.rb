b1247f2a384ac053436682ed515af0fdb511b961
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-16 09:44:36 -0400

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,6 +34,24 @@ class ApplicationController < ActionController::Base
     current_account.persisted? && !current_account.is_public?
   end
 
+  # Override method from devise-guests v0.7.0 to prevent the application
+  # from attempting to create duplicate guest users
+  def guest_user
+    return @guest_user if @guest_user
+    if session[:guest_user_id]
+      # Override - added #unscoped to include guest users who are filtered out of User queries by default
+      @guest_user = User.unscoped.find_by(User.authentication_keys.first => session[:guest_user_id]) rescue nil
+      @guest_user = nil if @guest_user.respond_to? :guest and !@guest_user.guest
+    end
+    @guest_user ||= begin
+                      u = create_guest_user(session[:guest_user_id])
+                      session[:guest_user_id] = u.send(User.authentication_keys.first)
+                      u
+                    end
+    @guest_user
+  end
+
+
   def api_or_pdf?
     request.format.to_s.match('json') ||
       params[:print] ||
--- app/models/concerns/bulkrax/has_matchers_decorator.rb
+++ app/models/concerns/bulkrax/has_matchers_decorator.rb
@@ -2,60 +2,59 @@
 
 # OVERRIDE Bulkrax v5.3.0 to add a geonames lookup for the `based_near` proprerty
 
-module Hyku
-  module Bulkrax
-    module HasMatchersDecorator
-      ##
-      # @note We likely want to extract something like this to Bulkrax.
-      #       However, we probably cannot assume that we have a field named
-      #       `based_near` that is in fact the Location field.
-      #
-      #       On possibility is to have a Hash that has key of :name and value
-      #       of a lambda; that Lambdawould receive the given :result and return
-      #       the correct value.  As we can see in the implementation below, we
-      #       need to consider the `Site.instance` which is a Hyku specific
-      #       consideration.
-      def matched_metadata(multiple, name, result, object_multiple)
-        if name == 'based_near'
-          return result if result.blank?
+module Bulkrax
+  module HasMatchersDecorator
+    ##
+    # @note We likely want to extract something like this to Bulkrax.
+    #       However, we probably cannot assume that we have a field named
+    #       `based_near` that is in fact the Location field.
+    #
+    #       On possibility is to have a Hash that has key of :name and value
+    #       of a lambda; that Lambdawould receive the given :result and return
+    #       the correct value.  As we can see in the implementation below, we
+    #       need to consider the `Site.instance` which is a Hyku specific
+    #       consideration.
+    def matched_metadata(multiple, name, result, object_multiple)
+      if name == 'based_near'
+        return result if result.blank?
 
-          result = if result.start_with?('http')
-                     Hyrax::ControlledVocabularies::Location.new(RDF::URI.new(result))
-                   else
-                     geonames_lookup(result)
-                   end
-        end
-        super
+        result = if result.start_with?('http')
+                   Hyrax::ControlledVocabularies::Location.new(RDF::URI.new(result))
+                 else
+                   geonames_lookup(result)
+                 end
+      end
+      super
+    end
+
+    private
+
+    def geonames_lookup(result)
+      geonames_username = ::Site.instance.account.geonames_username
+      return nil unless geonames_username
+
+      base_url = 'http://api.geonames.org/searchJSON'
+      params = { q: result, maxRows: 10, username: geonames_username }
+      uri = URI(base_url)
+      uri.query = URI.encode_www_form(params)
+
+      response = Net::HTTP.get_response(uri)
+      data = JSON.parse(response.body)
+      geoname = data['geonames'].first
+
+      unless geoname
+        uri = URI::HTTP.build(host: 'fake', fragment: result)
+        return Hyrax::ControlledVocabularies::Location.new(RDF::URI.new(uri))
       end
 
-      private
-
-      def geonames_lookup(result)
-        geonames_username = ::Site.instance.account.geonames_username
-        return nil unless geonames_username
-
-        base_url = 'http://api.geonames.org/searchJSON'
-        params = { q: result, maxRows: 10, username: geonames_username }
-        uri = URI(base_url)
-        uri.query = URI.encode_www_form(params)
-
-        response = Net::HTTP.get_response(uri)
-        data = JSON.parse(response.body)
-        geoname = data['geonames'].first
-
-        unless geoname
-          uri = URI::HTTP.build(host: 'fake', fragment: result)
-          return Hyrax::ControlledVocabularies::Location.new(RDF::URI.new(uri))
-        end
-
-        # Create a Hyrax::ControlledVocabularies::Location object with the RDF subject
-        rdf_subject = RDF::URI.new("https://sws.geonames.org/#{geoname['geonameId']}/")
-        Hyrax::ControlledVocabularies::Location.new(rdf_subject)
-      end
+      # Create a Hyrax::ControlledVocabularies::Location object with the RDF subject
+      rdf_subject = RDF::URI.new("https://sws.geonames.org/#{geoname['geonameId']}/")
+      Hyrax::ControlledVocabularies::Location.new(rdf_subject)
     end
   end
 end
 
+
 # Prepending this to `Bulkrax::HasMatchers` yielded an unbound method
 # Thus, I am prepending it to `Bulkrax::Entry` since that mixes in `Bulkrax::HasMatchers`
-Bulkrax::Entry.prepend(Hyku::Bulkrax::HasMatchersDecorator)
+Bulkrax::Entry.prepend(Bulkrax::HasMatchersDecorator)
