fe304b339c1b1f2ef0e75fb8152d15ca0bebe153
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-17 11:14:38 -0400

    ☑️ Updating test to include CSV for imports

--- spec/services/import_counter_metrics_spec.rb
+++ spec/services/import_counter_metrics_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 RSpec.describe ImportCounterMetrics do
   # TODO: Write more robust tests for this service
   it 'creates Hyrax::CounterMetrics with investigations' do
-    ImportCounterMetrics.import_investigations('spec/fixtures/csv/pittir-views.csv')
+    ImportCounterMetrics.import_investigations(Rails.root.join('spec/fixtures/csv/pittir-views.csv').to_s)
     expect(Hyrax::CounterMetric.count).not_to be_nil
   end
 
   it 'creates Hyrax::CounterMetrics with requests' do
-    ImportCounterMetrics.import_requests('spec/fixtures/csv/pittir-downloads.csv')
+    ImportCounterMetrics.import_requests(Rails.root.join('spec/fixtures/csv/pittir-views.csv').to_s)
     expect(Hyrax::CounterMetric.count).not_to be_nil
   end
 end
