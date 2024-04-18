faded8ca15202008b0ca45d4ddd0f7fd93f19fa3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-18 12:27:27 -0400

    ♻️ Ignore case where we don't have an account

--- app/models/ability_decorator.rb
+++ app/models/ability_decorator.rb
@@ -3,7 +3,12 @@
 # OVERRIDE ability to download files when the site disallows downloads.
 module AbilityDecorator
   def test_download(*args)
-    if Site.account.settings[:allow_downloads].nil? || Site.account.settings[:allow_downloads].to_i.nonzero?
+    account = Site.account
+
+    # In cases where we don't have an account.
+    return super unless account
+
+    if account.settings[:allow_downloads].nil? || account.settings[:allow_downloads].to_i.nonzero?
       super
     else
       false
