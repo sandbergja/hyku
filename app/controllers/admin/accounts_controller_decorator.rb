e736c4cba18035e37f3744ce72e4fa16b37bfd61
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-18 12:25:06 -0400

    Remove method call to is_superadmin

--- app/controllers/admin/accounts_controller_decorator.rb
+++ app/controllers/admin/accounts_controller_decorator.rb
@@ -6,7 +6,7 @@ module Admin
       params.require(:account).permit(:name, :cname, :title,
                                       *@account.public_settings(
                                         # OVERRIDE passing is_superadmin
-                                        is_superadmin: current_user.is_superadmin
+                                        is_superadmin: current_user.superadmin?
                                       ).keys)
     end
   end
