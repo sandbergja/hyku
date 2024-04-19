6abed9706a5b98a66f18fcc0ba3bfdfadc2a4789
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2024-04-19 09:12:59 -0400

    Removing file not needed

--- app/jobs/file_set_index_job.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class FileSetIndexJob < Hyrax::ApplicationJob
-  def perform(file_set)
-    return unless file_set
-
-    Hyrax.index_adapter.save(resource: file_set)
-  end
-end
