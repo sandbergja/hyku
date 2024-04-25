405545d0e88e27451d4b267a62fb1cc0b3fef005
Shana Moore <shana@scientist.com>
2024-04-25 11:56:14 -0700

    ♻️ revert changes to authentication to use default key instead

--- config/initializers/authentication.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-ApplicationController.http_basic_auth_username = ENV['BASIC_AUTH_USERNAME'] || 'samvera'
-ApplicationController.http_basic_auth_password = ENV['BASIC_AUTH_PASSWORD'] || 'hyku'
