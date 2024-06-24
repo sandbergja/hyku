ea2cae2f8d30c7ea54e01e549a4d817491f6e8da
LaRita Robinson <laritakr@users.noreply.github.com>
2024-06-24 12:40:50 -0400

    Fix duplicate thumbnails for PDFs (#2254)
    * Fix duplicate thumbnails for PDFs
    
    When text extraction fails on a PDF, the derivative job resubmits. This
    was results in 5 Hyrax::FileMetadata objects for the same thumbnail
    file, because jobs automatically resubmit 5 times.
    
    In this PR, I commented out the failing extract_full_text method. I
    also qualified the thumbnail creation on whether the thumbnail is already
    there, in cases where we run more after creating the thumbnail.
    
    * Rubocop fixes

--- app/services/hyrax/file_set_derivatives_service_decorator.rb
+++ app/services/hyrax/file_set_derivatives_service_decorator.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
-# OVERRIDE Hyrax v5.0.0rc2 to increase the size of thumbnails
-
+# OVERRIDE Hyrax v5.0.0rc2
+# - to increase the size of thumbnails
+# - add method missing_thumbnail? for methods which do both thumbnail AND another derivative
+#   (We were getting duplicate Hyrax::Metadata for thumbnails due to failing and rerunning derivative jobs.)
+# - comment out failing method extract_full_text for PDF derivatives
 module Hyrax
   module FileSetDerivativesServiceDecorator
     # @see https://github.com/samvera/hydra-derivatives/blob/main/lib/hydra/derivatives/processors/video/config.rb#L59
     DEFAULT_VIDEO_SIZE = '320x240'
+
+    ## Override initialize to add method missing_thumbnail?
+    #  We were getting duplicate Hyrax::Metadata for thumbnails due to failing and rerunning derivative jobs.
+    def initialize(file_set)
+      super
+      @fs = if @file_set.is_a?(Hyrax::FileMetadata)
+              Hyrax.query_service.find_by(id: @file_set.file_set_id)
+            else
+              @file_set
+            end
+    end
+
+    def missing_thumbnail?
+      @fs.thumbnail.nil?
+    end
 
     def create_pdf_derivatives(filename)
       Hydra::Derivatives::PdfDerivatives.create(filename, outputs: [{ label: :thumbnail,
@@ -13,15 +31,21 @@ module Hyrax
                                                                       size: '676x986',
                                                                       url: derivative_url('thumbnail'),
                                                                       layer: 0 }])
-      extract_full_text(filename, uri)
+
+      # @todo: Fix extract_full_text & wrap above in if missing_thumbnail? check
+      #        commented out because it is failing resulting in RuntimeError (blank file detected)
+      #        in ValkyriePersistDerivatives
+      # extract_full_text(filename, uri)
     end
 
     def create_office_document_derivatives(filename)
-      Hydra::Derivatives::DocumentDerivatives.create(filename, outputs: [{ label: :thumbnail,
-                                                                           format: 'jpg',
-                                                                           size: '600x450>',
-                                                                           url: derivative_url('thumbnail'),
-                                                                           layer: 0 }])
+      if missing_thumbnail?
+        Hydra::Derivatives::DocumentDerivatives.create(filename, outputs: [{ label: :thumbnail,
+                                                                             format: 'jpg',
+                                                                             size: '600x450>',
+                                                                             url: derivative_url('thumbnail'),
+                                                                             layer: 0 }])
+      end
       extract_full_text(filename, uri)
     end
 
