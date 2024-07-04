e890b9f4c6c12fb6ee841c89a0a8608b4725cef9
Kirk Wang <kirk.wang@scientist.com>
2024-07-04 07:46:40 -0700

    Add explicit requires for riiif constants
    In development we were seeing the Universal Viewer locking up the entire
    web service when trying to load certain riiif constants.  This commit
    will load those contants beforehand in an initializer.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
+
+# Loading these constants during runtime would freeze the web service in development.
+# Adding the requires here to load them on initialize.
+require 'riiif/imagemagick_transformer'
+require 'riiif/imagemagick_command_factory'
+require 'riiif/crop'
+require 'riiif/resize'
+
 Rails.application.reloader.to_prepare do
   Riiif::Image.info_service = lambda do |id, _file|
     # id will look like a path to a pcdm:file
