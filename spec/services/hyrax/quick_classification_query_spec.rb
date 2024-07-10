d7f9b85c1daa9b294178870329950210b6b06c6e
Kirk Wang <kirk.wang@scientist.com>
2024-07-09 22:48:39 -0700

    ✅ Fix QuickClassificationQuery leaky spec
    The Hyrax::QuickClassificationQuery spec was leaking into other specs
    because the user ability was being loaded with just the GenericWork.
    Loading Hyrax::ModelRegistry.work_classes seems to ensure things are
    loaded correctly.

--- spec/services/hyrax/quick_classification_query_spec.rb
+++ spec/services/hyrax/quick_classification_query_spec.rb
@@ -2,6 +2,8 @@
 
 # OVERRIDE Hyrax v2.9.0
 RSpec.describe Hyrax::QuickClassificationQuery, clean_repo: true do
+  # Ensure Hyrax::ModelRegistry.work_classes is loaded so this spec doesn't leak into other specs
+  let!(:work_classes) { Hyrax::ModelRegistry.work_classes }
   # OVERRIDE: add :work_depositor role -- proper testing requires create permission
   let(:user) { create(:user, roles: [:work_depositor]) }
 
